740da053699200d65bbd04e3f3128e10a5b1f673
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-10-04 22:41:37 +0900

    Update theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,9 @@
 import { createTheme } from '@mui/material'
 
-export const theme = createTheme()
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#111111',
+    },
+  },
+})
